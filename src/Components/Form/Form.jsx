e384cddb1cdc2bc92cf30bad468fa7d5336a3122
Osman Herman <osmanherman27@gmail.com>
2023-04-04 22:48:21 -0300

    Subida

---
+++ src/Components/Form/Form.jsx
@@ -0,0 +1,11 @@
+const Form = () => {
+    return (
+        <div>
+            <form action="">
+                <input type="text" />
+            </form>
+        </div>
+    )
+}
+
+export default Form
